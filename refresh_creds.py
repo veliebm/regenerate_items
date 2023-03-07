20c01e7434e679f19ba395d2517cbe689daf866c
veliebm <veliebm@gmail.com>
2023-03-07 16:55:07 -0600

    Raise error if key not present

--- refresh_creds.py
+++ refresh_creds.py
@@ -11,8 +11,11 @@ ENCRYPTED_PASSWORD_PATH = "/tmp/encrypted_amazon_password"
 PASSWORD_PROMPT = "Please enter your Amazon password. This will be deleted from the system upon reboot: "
 ENCODING = "utf-8"
 
+try:
+    key = pathlib.Path(KEY_PATH).read_text()
+except OSError:
+    raise RuntimeError("Please generate a key before running this script")
 
-key = pathlib.Path(KEY_PATH).read_text()
 reference_key = fernet.Fernet(key)
 
 encrypted_password = reference_key.encrypt(
