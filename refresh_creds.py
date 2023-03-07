d91f1f31f6801ec81e51bb686f1d0094623e41c2
veliebm <veliebm@gmail.com>
2023-03-06 21:23:36 -0600

    Change key strat to just regen encrypted pass

---
+++ refresh_creds.py
@@ -0,0 +1,27 @@
+"""
+Refreshes your Amazon credentials by asking you for your password.
+"""
+
+from cryptography import fernet
+import pathlib
+import getpass
+
+KEY_PATH = "key"
+ENCRYPTED_PASSWORD_PATH = "/tmp/encrypted_amazon_password"
+PASSWORD_PROMPT = (
+    "Please enter your password. This will be deleted from the system upon reboot: "
+)
+ENCODING = "utf-8"
+
+
+key = pathlib.Path(KEY_PATH).read_text()
+reference_key = fernet.Fernet(key)
+
+encrypted_password = reference_key.encrypt(
+    bytes(
+        getpass.getpass(PASSWORD_PROMPT),
+        ENCODING,
+    )
+)
+
+pathlib.Path(ENCRYPTED_PASSWORD_PATH).write_text(str(encrypted_password, ENCODING))
